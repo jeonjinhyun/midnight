8cfe1d56c78ca2d752b77cd1cb60c537c98c0b20
jeonjinhyun <jjh06500@gmail.com>
2024-08-19 10:34:40 +0900

    0819

---
+++ src/main/java/com/ohgiraffers/midnight/MidnightApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.midnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidnightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/midnight/MidnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.midnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidnightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
